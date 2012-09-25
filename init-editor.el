295c1e5f30f349172149067678bdf046240244db
Mykhaylo Sorochan <msorc@yandex.ru>
2012-09-25 17:01:42 +0300

    Don't use tabs

--- init-editor.el
+++ init-editor.el
@@ -1,11 +1,12 @@
-
-
 ;; Interactively Do Things
 (require 'ido)
 (ido-mode t)
 ; if the entered string does not match any buffer name, any buffer name containing
 ; the entered characters in the given sequence will match.
 (setq ido-enable-flex-matching t)
+
+;; don't use tabs
+(setq-default indent-tabs-mode nil)
 
 ;; editor configs
 (setq make-backup-files nil)
