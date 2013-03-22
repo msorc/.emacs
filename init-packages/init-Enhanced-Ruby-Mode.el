0af30d27c4d7039090096d8de57b9964fb396212
Mykhaylo Sorochan <msorc@yandex.ru>
2013-03-22 17:02:02 +0200

    Map more Ruby related files

---
+++ init-packages/init-Enhanced-Ruby-Mode.el
@@ -0,0 +1,5 @@
+;; map more ruby related files
+(add-to-list 'auto-mode-alist '(("\\Gemfile$" . ruby-mode)
+                                ("\\.rake$" . ruby-mode)
+                                ("\\.gemspec$" . ruby-mode)
+                                ("\\.thor$" . ruby-mode))
