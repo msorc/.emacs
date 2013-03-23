2be76acc4c6c2362e8aa51ee4347828553056708
Mykhaylo Sorochan <msorc@yandex.ru>
2013-03-23 23:01:31 +0200

    Fix ruby-mode

--- init-packages/init-Enhanced-Ruby-Mode.el
+++ init-packages/init-Enhanced-Ruby-Mode.el
@@ -1,5 +1,5 @@
 ;; map more ruby related files
-(add-to-list 'auto-mode-alist '(("\\Gemfile$" . ruby-mode)
-                                ("\\.rake$" . ruby-mode)
-                                ("\\.gemspec$" . ruby-mode)
-                                ("\\.thor$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
