597e4cb69cc8e94d8b38f4dcdfb1805129a9f0db
Mykhaylo Sorochan <msorc@yandex.ru>
2013-10-07 16:02:41 +0300

    * init-packages/init-Enhanced-Ruby-Mode.el: Rakefile to ruby mode

--- init-packages/init-Enhanced-Ruby-Mode.el
+++ init-packages/init-Enhanced-Ruby-Mode.el
@@ -1,5 +1,6 @@
 ;; map more ruby related files
 (add-to-list 'auto-mode-alist '("\\Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
