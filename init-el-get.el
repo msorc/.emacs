64932bfe1783903f778be374dd1dae488f5d14de
Mykhaylo Sorochan <msorc@yandex.ru>
2015-07-12 14:11:45 +0400

    changes

--- init-el-get.el
+++ init-el-get.el
@@ -3,13 +3,14 @@
 ; Packages initialization files
 (setq el-get-user-package-directory "~/.emacs.d/init-packages")
 
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (let (el-get-master-branch)
-       (goto-char (point-max))
-       (eval-print-last-sexp)))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 
 ; Packages
 (setq my-packages
@@ -37,20 +38,22 @@
 	;; programming
         projectile      ; project organising
 	coffee-mode 	; coffescript
-	css-mode nxhtml ; html & css
+        livescript-mode ; livescript
+	css-mode ;nxhtml ; html & css
 	magit           ; git
 	js2-mode        ; javascript
 	paredit slime 	; lisp
 	sicp            ; scheme
+        skewer-mode trident-mode ; js live
         cmake-mode
 	; ruby
 	Enhanced-Ruby-Mode ;flymake-ruby 
         ruby-block ruby-end ruby-electric
-	rdebug rspec-mode
+	ruby-debug rspec-mode
 	rvm
 	; rails
         ;rinari 
-        rhtml-mode haml-mode sass-mode ;slim-mode
+        rhtml-mode haml-mode sass-mode slim-mode
 	feature-mode 	; cucumber
 	yaml-mode 	; yaml
 	))
