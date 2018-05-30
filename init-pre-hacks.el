cea35d773336f5f1a5509ccc3f20e7874a6041b4
Mykhaylo Sorochan <msorc@yandex.ru>
2018-05-30 10:48:53 +0400

    changes

--- init-pre-hacks.el
+++ init-pre-hacks.el
@@ -1,8 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d/custom-packages")
-; rcodetools
-(require 'rcodetools)
-; haxe
-(require 'haxe-mode)
 ; txl
 (require 'txl-mode)
 (add-to-list 'auto-mode-alist '("\\.\\([tT]xl\\|[gG]rm\\|[gG]rammar\\|[rR]ul\\(es\\)?\\|[mM]od\\(ule\\)?\\)$" . txl-mode))
